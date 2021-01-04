08d549524a02e85418b874a494f054f1b45d48ae
nabinno <nab@blahfe.com>
2021-01-05 07:49:29 +0900

    Fix `post.fields.slug`.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -29,10 +29,10 @@ const BlogIndex = ({ data, location }) => {
       <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+          const title = post.frontmatter.title || post.fields?.slug
 
           return (
-            <li key={post.fields.slug}>
+            <li key={post.fields?.slug}>
               <article
                 className="post-list-item"
                 itemScope
@@ -40,7 +40,7 @@ const BlogIndex = ({ data, location }) => {
               >
                 <header>
                   <h2>
-                    <Link to={post.fields.slug} itemProp="url">
+                    <Link to={post.fields?.slug} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
